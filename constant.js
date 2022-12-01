4b0c5f9b683fc5a1898bb1c34b1ef417f79e50ff
Thuongnn1 <thuongnn@minori.com.vn>
2022-12-01 11:05:34 +0700

    123

---
+++ constant.js
@@ -0,0 +1,10 @@
+export const rqUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
